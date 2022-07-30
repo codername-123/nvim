55f74c06c8768fc2663c7c9432416c5ee7afdbe1
codername-123 <nitinr629@gmail.com>
2022-07-30 11:57:42 +0530

    added support for completions

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -38,7 +38,20 @@ return packer.startup({function(use)
 
   -- Treesitter
   use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
-  
+
+  -- Completions
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-buffer"
+  use "hrsh7th/cmp-path"
+  use "hrsh7th/cmp-cmdline"
+  use "hrsh7th/nvim-cmp"
+  use "onsails/lspkind-nvim"
+  -- Snippets
+  use "L3MON4D3/LuaSnip"
+  use "saadparwaiz1/cmp_luasnip"
+  use "rafamadriz/friendly-snippets"
+
+
   if packer_bootstrap then
     require('packer').sync()
   end 
